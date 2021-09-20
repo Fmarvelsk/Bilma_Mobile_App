37d7aba4636156c262492525e9df93fc6896850d
fmarvelsk <fmarvelsk@gmail.com>
2021-09-20 21:27:34 +0100

    sign up page

--- App.js
+++ App.js
@@ -1,12 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View, Image, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [signUp, setSignUp] = useState(false)
+
   return (
     <View style={styles.container}>
-      <Text>This is where the magic works!</Text>
+      {signUp ? "" :
+      (<>
+      <Text style={styles.headerText}>Welcome To Bilma!</Text>
+      <Image style={{
+        marginBottom: 30
+      }} source={require('./assets/profile.png')} />
+      <View style={styles.login}>
+        <TextInput
+          style={styles.input}
+          onChange={text => setUsername(text)}
+          placeholder="Username"
+        />
+      </View>
+      <View style={styles.login}>
+        <TextInput
+          onChangeText={text => setPassword(text)}
+          style={styles.input}
+          placeholder="Password"
+        />
+      </View>
+      <TouchableOpacity style={styles.btnView}>
+
+        <Text style={styles.loginText}>LOGIN</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={{
+          height: 30,
+          marginTop: 10,
+        }}> Forgot Password ?</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
+</>)}
     </View>
   );
 }
@@ -14,8 +48,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#D7D7D7',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  login: {
+    borderRadius: 30,
+    width: "60%",
+    height: 45,
+    backgroundColor: 'transparent',
+    borderWidth: 1,
+    marginBottom: 20,
+
+  },
+  input: {
+    padding: 10,
+    height: 50,
+    flex: 1,
+    marginLeft: 8
+
+  },
+  headerText: {
+    color: '#ff6666',
+    marginBottom: 24,
+    fontSize: 23
+  },
+  btnView: {
+    width: '60%',
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#FF1493",
+  },
+
 });
