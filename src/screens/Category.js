06ee122bceec97874ca2c4f3f9cecbb02d7bb6eb
fmarvelsk <fmarvelsk@gmail.com>
2021-12-06 14:46:29 +0100

    request

--- src/screens/Category.js
+++ src/screens/Category.js
@@ -1,6 +1,6 @@
 import { LinearGradient } from "expo-linear-gradient";
 import React, { useCallback, useEffect, useState } from "react";
-import { View, Text, StyleSheet, ScrollView, TextInput, TouchableOpacity, TouchableWithoutFeedback, Image, ActivityIndicator } from "react-native";
+import { View, Text, StyleSheet, FlatList, ScrollView, TextInput, TouchableOpacity, TouchableWithoutFeedback, Image, ActivityIndicator } from "react-native";
 import HomeHeaderView from "../component/HomeHeaderView";
 import Header from '../component/Header'
 import pic from '../assets/pic.png'
@@ -12,22 +12,29 @@ const STAR = require('../assets/star.png')
 
 export default function Category({ navigation }) {
     const [loading, setLoading] = useState(false)
-    const sel = useSelector(s => s)
-    console.log(sel)
+    const { category } = useSelector(s => s.rootReducer)
+    const [categoryList, setCategoryList] = useState()
+
     const fetchCategory = useCallback(async () => {
         setLoading(true)
         try {
-            let res = await db.collection("data").where('Name', '==', 'Hall').get()
-            res.forEach(doc => {
-                console.log(doc.id, '=>', doc.data());
-            });
-
+           await db.collection('data').where('Category', '==', category)
+                .get().then((snapshot) => {
+                    let data = snapshot.docs.map((snap) => {
+                        const result = snap.data()
+                        const id = snap.id
+                        return { result, id }
+                    })
+                    setCategoryList(data)
+                })
         }
         catch (err) {
             console.log(err)
         }
         setLoading(false)
-    }, [sel])
+    }, [category])
+
+console.log(categoryList)
 
     useEffect(() => {
         fetchCategory()
@@ -36,83 +43,46 @@ export default function Category({ navigation }) {
     return (
         <HomeHeaderView navigation={navigation}>
             {!loading ?
-               (     
-                    
-               <View>
-                    <Header>
-                        Recommeded
-                    </Header>
-                    <TouchableWithoutFeedback onPress={() => navigation.push('CategoryId')}>
-                        <View style={style.category} >
+                (
+
+                    <View>
+                        <Header>
+                            Recommeded
+                        </Header>
+                        <FlatList renderItem={({item}) => (
+
+                        <View style={style.category}>
                             <Image style={style.img_prop} source={pic} />
-                            <View style={style.pos}>
-                                <Text>Rating</Text>
-                                <Rating
-                                    type="custom"
-                                    imageSize={10}
-                                    onFinishRating={(value) => console.log(value)}
-                                    style={{ paddingVertical: 10 }}
-                                />
-                                <Text>Rate</Text>
-
-                            </View>
-                        </View>
-                    </TouchableWithoutFeedback>
-
-                    <View style={style.category}>
-                        <Image style={style.img_prop} source={pic} />
-                        <View style={style.textside}>
-                            <View>
-                                <Text style={{ fontSize: 14 }}>Rating</Text>
-                                <View style={{ flexDirection: "row", alignItems: 'center' }}>
-                                    <Rating
-                                        type="custom"
-                                        imageSize={14}
-                                        ratingImage={STAR}
-                                        style={{ paddingVertical: 10, left: -4 }}
-                                        ratingBackgroundColor="#f4f4f4"
-                                        ratingColor="blue"
-                                        ratingCount={1}
-                                        readonly={true}
-                                    />
-                                    <Text>4.9</Text>
+                            <View style={style.textside}>
+                                <View>
+                                    <Text style={{ fontSize: 14 }}>Rating</Text>
+                                    <View style={{ flexDirection: "row", alignItems: 'center' }}>
+                                        <Rating
+                                            type="custom"
+                                            imageSize={14}
+                                            ratingImage={STAR}
+                                            style={{ paddingVertical: 10, left: -4 }}
+                                            ratingBackgroundColor="#f4f4f4"
+                                            ratingColor="blue"
+                                            ratingCount={1}
+                                            readonly={true}
+                                        />
+                                        <Text>4.9</Text>
+                                    </View>
+                                </View>
+                                <View>
+                                    <Text style={{ fontSize: 14 }}>Rate</Text>
                                 </View>
                             </View>
-                            <View>
-                                <Text style={{ fontSize: 14 }}>Rate</Text>
-                            </View>
                         </View>
-                    </View>
+                        )}/>
+                 
 
-                    <View style={style.category}>
-                        <Image style={style.img_prop} source={pic} />
-                        <View style={style.textside}>
-                            <View>
-                                <Text style={{ fontSize: 14 }}>Rating</Text>
-                                <View style={{ flexDirection: "row", alignItems: 'center' }}>
-                                    <Rating
-                                        type="custom"
-                                        imageSize={14}
-                                        ratingImage={STAR}
-                                        style={{ paddingVertical: 10, left: -4 }}
-                                        ratingBackgroundColor="#f4f4f4"
-                                        ratingColor="blue"
-                                        ratingCount={1}
-                                        readonly={true}
-                                    />
-                                    <Text>4.9</Text>
-                                </View>
-                            </View>
-                            <View>
-                                <Text style={{ fontSize: 14 }}>Rate</Text>
-                            </View>
-                        </View>
+     
                     </View>
-         
-                </View>      
-      )
+                )
                 : (<><ActivityIndicator size="large" color="#0000ff" />
-                     </>)}
+                </>)}
         </HomeHeaderView>
     )
 }
